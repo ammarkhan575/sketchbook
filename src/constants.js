b72a5cc6a199e99c3e8f7cefaeea0cd7e127ebd4
ammar <ammarkhan575@gmail.com>
2024-02-22 16:42:58 +0530

    toolbox and menubar complete

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const COLORS = {
+    BLACK: 'black',
+    WHITE: 'white',
+    GREEN: 'green',
+    RED: 'red',
+    BLUE: 'blue',
+    YELLOW: 'yellow',
+    ORANGE: 'orange',
+}
+
+export {COLORS};
