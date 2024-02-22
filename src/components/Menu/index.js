293a4f997e02e99bf11a668f6827a21e77af8c44
ammar <ammarkhan575@gmail.com>
2024-02-22 19:20:38 +0530

    data share between menu and toolbar using redux toolkit

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 import styles from './index.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { MENU_ITEMS } from '@/constants';
+import { menuItemClick, actionItemClick } from '@/slice/menuSlice';
+import cx from 'classnames';
 
 const Menu = () => {
+    const dispatch = useDispatch();
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+    const handleMenuClick = (option) => {
+        dispatch(menuItemClick(option));
+    }
     return (
         <div className={styles.menuContainer}>
-            <div className={styles.menuItem}>
+            <div className={cx(styles.menuItem, { [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL })} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
                 <FontAwesomeIcon icon={faPencil} />
             </div>
-            <div className={styles.menuItem}>
+            <div className={cx(styles.menuItem, { [styles.active]: activeMenuItem === MENU_ITEMS.ERASER })} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
                 <FontAwesomeIcon icon={faEraser} />
             </div>
             <div className={styles.menuItem}>
@@ -20,7 +29,7 @@ const Menu = () => {
             <div className={styles.menuItem}>
                 <FontAwesomeIcon icon={faFileArrowDown} />
             </div>
-        </div>
+        </div >
     )
 }
 
