1ad302ffa1378c5bd72474443e016f99ca24d0d9
ammar <ammarkhan575@gmail.com>
2024-02-24 00:49:46 +0530

    some comment add

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -17,27 +17,35 @@ const Menu = () => {
     }
     return (
         <div className={styles.menuContainer}>
+            {/* PENCIL */}
             <div className={cx(styles.menuItem, { [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL })} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
                 <FontAwesomeIcon icon={faPencil} />
             </div>
+            {/* RECTANGLE */}
             <div className={cx(styles.menuItem, { [styles.active]: activeMenuItem === MENU_ITEMS.RECTANGLE })} onClick={() => handleMenuClick(MENU_ITEMS.RECTANGLE)}>
                 <div className='w-5 h-3 bg-white border border-black'></div>
             </div>
+            {/* CIRCLE */}
             <div className={cx(styles.menuItem, { [styles.active]: activeMenuItem === MENU_ITEMS.CIRCLE })} onClick={() => handleMenuClick(MENU_ITEMS.CIRCLE)}>
                 <div className='w-5 h-5 bg-white border rounded-full border-black'></div>
             </div>
+            {/* LINE */}
             <div className={cx(styles.menuItem, { [styles.active]: activeMenuItem === MENU_ITEMS.LINE })} onClick={() => handleMenuClick(MENU_ITEMS.LINE)}>
                 <div className='w-5 h-[1px] bg-white border -rotate-45 rounded-full border-black'></div>
             </div>
+            {/* ERASER */}
             <div className={cx(styles.menuItem, { [styles.active]: activeMenuItem === MENU_ITEMS.ERASER })} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
                 <FontAwesomeIcon icon={faEraser} />
             </div>
+            {/* UNDO */}
             <div className={styles.menuItem} onClick={() => handleActionItemClick(MENU_ITEMS.UNDO)}>
                 <FontAwesomeIcon icon={faRotateLeft} />
             </div>
+            {/* REDO */}
             <div className={styles.menuItem} onClick={() => handleActionItemClick(MENU_ITEMS.REDO)}>
                 <FontAwesomeIcon icon={faRotateRight} />
             </div>
+            {/* DOWNLOAD */}
             <div className={styles.menuItem} onClick={() => handleActionItemClick(MENU_ITEMS.DOWNLOAD)}>
                 <FontAwesomeIcon icon={faFileArrowDown} />
             </div>
